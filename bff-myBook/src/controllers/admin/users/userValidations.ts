55b0208df6d5cbd41cda9d7aa6ead85d1fba6d00
supra-jatinjainsaraf <j.saraf@supraoracles.com>
2024-02-09 12:13:33 +0530

    model update

--- bff-myBook/src/controllers/admin/users/userValidations.ts
+++ bff-myBook/src/controllers/admin/users/userValidations.ts
@@ -3,5 +3,5 @@ import Joi from 'joi'
 export const checkAdminValidator = Joi.object().keys({
     name: Joi.string().messages({"any.requires":"Please provide valid name"}),
     email: Joi.string().email().required(),
-    password: Joi.string().required(), 
+    password: Joi.string().min(8).required(), 
 })
--- bff-myBook/src/models/user/index.ts
+++ bff-myBook/src/models/user/index.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String},
+  name: { type: String, required: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  streetAddress: {type: String},
-  postalCode: {type: Number},
-  city: {type: String},
-  country: {type: String},
-  phone: {type: String},
-  isAdmin: {type: Boolean, default: true},
+  address: {type: String, required: true},
+  zipCode: {type: Number, required: true},
+  city: {type: String, required: true},
+  country: {type: String,required: true},
+  phone: {type: String,required: true},
+  isAdmin: {type: Boolean, default: false},
 }, { timestamps: true });
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
 export default User
