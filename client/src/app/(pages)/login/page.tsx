08313d31e32358d4cd858d32268b2b4c3ea8da9a
supra-jatinjainsaraf <j.saraf@supraoracles.com>
2024-02-09 12:26:37 +0530

    Client Pages and component setup

---
+++ client/src/app/(pages)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginComponent from '../../components/LoginPageComponent'
+
+const LoginPage = () => {
+  return (
+    <div>
+     <LoginComponent />
+    </div>
+  )
+}
+
+export default LoginPage
