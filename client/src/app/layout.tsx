0718849ff4e24b957a38725dfaa8c1900b2ef6aa
supra-jatinjainsaraf <j.saraf@supraoracles.com>
2024-02-09 12:35:12 +0530

    layout updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Book",
+  title: "My Book Client",
   description: "New Generation Social site",
 };
 
